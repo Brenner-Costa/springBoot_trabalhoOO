f48216d98e2c767c52346dd297a36d307cfb00e6
Brenner Costa <costabrenner1@gmail.com>
2025-04-13 22:14:45 -0300

    Iniciando o projeto

---
+++ trabalhoSpringOOLogin/src/test/java/com/example/trabalhoSpringOOLogin/TrabalhoSpringOoLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalhoSpringOOLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoSpringOoLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
